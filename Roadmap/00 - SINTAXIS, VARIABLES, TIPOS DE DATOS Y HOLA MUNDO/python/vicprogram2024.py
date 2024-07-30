862c15d5ae0d8448c38d5320e6f3b734fae56529
vicprogram2024 <168384662+vicprogram2024@users.noreply.github.com>
2024-07-30 19:22:12 -0400

    00 Sintaxis

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/vicprogram2024.py
@@ -0,0 +1,24 @@
+# Comentario en una linea. https://www.python.org
+
+"""Para el comentario en varias lineas 
+se utiliza tres comillas seguidas"""
+
+# Para crear una variable basta con agregar el nombre seguido del igual
+
+my_variable= "Mi variable"
+
+MY_VARCONSTANT= "Mi variable constante"
+
+# Variables de tipos de datos primitivos 
+
+my_int = 7
+my_float = 7.7
+my_string = "Mi cadena de texto"
+my_bool = True
+my_bool = False
+
+# Imprimir terminal de texto: "Hola Python"
+
+print ("Hola Python")
+
+
---
+++ Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/python/vicprogram2024.py
@@ -0,0 +1,23 @@
+""" Operadores """
+
+# Operadores Aritmeticos
+
+#print (f"Suma 7 + 15 = {7 + 15}")
+num1 = 10
+num2 = 15
+resultado_suma = num1 + num2
+resultado_resta = num1 - num2
+resultado_multiplicacion = num1 * num2
+resultado_division = num1 / num2
+resultado_porcentaje = num1 % num2
+resultado_potencia = num1 ** num2
+resultado_diventero = num1 // num2
+
+print (f"La suma de 10 + 15 = {resultado_suma}")
+print (f"La resta de 10 - 15 = {resultado_resta}")
+print (f"La multiplicacion de 10 * 15 = {resultado_multiplicacion}")
+print (f"La division de 10 / 15 = {resultado_division}")
+print (f"El porcentaje de 10 % 15 = {resultado_porcentaje}")
+print (f"La pontencia de 10 ** 15 = {resultado_potencia}")
+print (f"La division de numero entero de 10 // 15 = {resultado_diventero}")
+
