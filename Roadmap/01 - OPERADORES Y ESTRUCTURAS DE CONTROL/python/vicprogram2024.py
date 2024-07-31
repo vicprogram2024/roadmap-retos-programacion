6e6901715e91a218ec323cf714bb1da1bc718ba4
vicprogram2024 <168384662+vicprogram2024@users.noreply.github.com>
2024-07-31 16:37:57 -0400

    FINAL

--- Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/python/vicprogram2024.py
+++ Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/python/vicprogram2024.py
@@ -1,10 +1,11 @@
 """ Operadores """
 
-# Operadores Aritmeticos
+# OPERADORES ARITMETICOS
 
 #print (f"Suma 7 + 15 = {7 + 15}")
 num1 = 10
 num2 = 15
+num3 = 55
 resultado_suma = num1 + num2
 resultado_resta = num1 - num2
 resultado_multiplicacion = num1 * num2
@@ -21,4 +22,86 @@ print (f"El porcentaje de 10 % 15 = {resultado_porcentaje}")
 print (f"La pontencia de 10 ** 15 = {resultado_potencia}")
 print (f"La division de numero entero de 10 // 15 = {resultado_diventero}")
 
-#
+#OPERADORES LOGICOS 
+
+""" Se utiliza para tomar decisiones basadas en multiples condiciones."""
+
+# Operador And 
+
+""" Evalua si los valores de ambos lados son verdaderos,si se cumple nos dara un true, en caso contrario sera false.
+
+(true and true) true
+(true and flase) false
+(false and true) false 
+(false and flase) false """
+
+#Operador Or
+
+"""Nos da true cuando al menos uno de los valores es verdadero.
+
+(true or true) true
+(true or flase) true
+(false or true) true 
+(false or flase) false"""
+
+# Operador not
+
+print (f"El numero 17 es mayor que 15 y 50 es mayor que 15: {17 > 15 and 50 > 15}")
+print (f"El numero 50 es menor que 15 or 17 es menor que 50 es: {50 < 15 or 17 < 50}")
+print (f"El numero 50 es menor que 15 or 17 es mayor que 50 es: {50 < 15 or 17 > 50}")
+print (f"El resultado de 10 + 9 == 20: { not 10 + 9 == 20}")
+
+# OPERADORES DE COMPARACION 
+
+""" Comparan o establecen una relacion entre ellos.
+Devolvera un valor true o false basado en una condicion """
+
+
+print (f"El numero 8 es mayor que el numero 7: {8 > 7}")
+print (f"El numero 7 es menor que el numero 9: {7 < 9}")
+print (f"El numero 7 es igual que el numero 8: {7 == 8}")
+print (f"El numero 7 es mayor o igual que el numero 8: {7 >= 8}")
+print (f"El numero 7 es menor o igual que el numero 9: {7 <= 9}")
+print (f"El numero 9 es diferente que el numero 7: {9 != 7}")
+
+#OPERADORES DE ASIGNACION 
+
+valor= 45
+print (f"Mi valor inicial es: {valor}")
+valor += 5
+print (f"Suma y asignacion es: {valor}")
+valor -= 5
+print (f"Resta y asignacion es: {valor}")
+valor *= 5
+print (f"Multiplicacion y asignacion es: {valor}")
+valor /=4
+print (f"Division y asignacion es: {valor}")
+valor %= 5
+print (f"Modulo y asignacion es: {valor}")
+valor **= 15
+print (f"Exponente y asignacion es: {valor}")
+valor //= 5
+print (f"Division entera y asignacion es: {valor}")
+
+#OPERADORES DE IDENTIDAD
+
+"""Sirven para comprobar si dos variables emplean la misma ubicacion en memoria
+
+Is nos da true si los operandos se refieren al mismo objeto.
+Is not da true si los operandos no se refieren al mismo objeto"""
+
+valor1= valor
+print (f"valor is valor1 es: {valor is valor1}")
+print (f"valor is not valor1 es: {valor is not valor1}")
+
+# OPERADORES DE PERTENECIA
+
+""" Identifica la pertenencia de un operador en una secuencia """
+
+print (f"La letra 'i' in 'Victor': {'i' in 'Victor'} ")
+print (f"La letra 'n' in not 'Victor': {'n' not in'Victor'} ")
+
+S
+
+
+
